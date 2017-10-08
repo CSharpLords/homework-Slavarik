d09a4d4b4bc26f6c044e950d9fa9d9efaac052ea
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-10-08 16:40:53 +0300

    Дракон

---
+++ Дракон/Дракон/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи возраст дракона");
+            int age = int.Parse(Console.ReadLine());
+            if (age < 150)
+            {
+                Console.WriteLine("Поздравляю!!!Вам попался молодой дракон");
+                Console.WriteLine("Введите его hp");
+                int hp = int.Parse(Console.ReadLine());
+                if (hp < 50)
+                {
+                    Console.WriteLine("Поздравляю с победой,юный рыцарь!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Слишком много hp,попытайся сбежать");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Лучше не подходить к дракону,он слишком мудр");
+                Console.ReadLine();
+            }
+                
+        }
+    }
+}
