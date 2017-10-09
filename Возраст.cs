39aadccbf74dfd47f93131d2020a282f9fbe08cf
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-10-09 21:52:16 +0300

    Возраст

---
+++ Возраст.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш возраст");
+            int age = int.Parse(Console.ReadLine());
+            int number = 10;
+            int number1 = 20;
+            if (number <= age)
+            {
+                Console.WriteLine("Вы подросток");
+                Console.ReadLine();
+                if ( number1 > age)
+                {
+                    Console.WriteLine("Вы не подросток");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
