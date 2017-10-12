38baca1ad90d6b2ff3a98e58332322ded932bcfd
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-10-12 21:54:52 +0300

    Кислород

---
+++ Кислород/Кислород/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Кислород
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите состав кислорода в воздухе(в процентах)");
+            int Oxygen = int.Parse(Console.ReadLine());
+            if (Oxygen > 20)
+            {
+                Console.WriteLine("Напишите среднюю температуру воздуха");
+                int t = int.Parse(Console.ReadLine());
+                Console.WriteLine("Отправляется сообщение");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Планета не пригодна для жизни");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Кислород/Кислород/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Кислород")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Кислород")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("644621a8-93bb-494e-bbd7-7423d3ef4907")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
