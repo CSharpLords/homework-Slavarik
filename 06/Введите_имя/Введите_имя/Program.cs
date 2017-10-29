1767cbbc78432ee39744f74d84c655f0ee1b2438
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-10-29 10:07:39 +0300

    Имя

---
+++ 06/Введите_имя/Введите_имя/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Введите_имя
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            int Name = int.Parse(Console.ReadLine());
+            SayHello("name");
+        }
+        static void SayHello(string Pull)
+        {
+            Console.WriteLine("Здарвстуйте,");
+            Console.ReadLine();
+        }
+    }
+
+}
