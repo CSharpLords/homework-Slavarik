ae223f5e2ae0d605d31b4d264b131c780c86db19
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-11-24 20:04:30 +0300

    Квадрат чисел

---
+++ 10/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите число:");
+                int a = int.Parse(Console.ReadLine());
+                int b = a * a;
+                Console.WriteLine("Квадрат числа равен:" + b);
+                Console.ReadLine();
+            }
+        }
+    }
+}
