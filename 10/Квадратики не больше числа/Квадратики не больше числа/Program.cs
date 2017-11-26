ca8bf13813721362825d201359911c7a6549c772
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-11-26 15:09:22 +0300

    Квадратики

---
+++ 10/Квадратики не больше числа/Квадратики не больше числа/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Квадраты_натуральных_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = int.Parse(Console.ReadLine());
+            int b = 1;
+            Console.WriteLine("Квадраты не превышающие:" + n);
+            while (true)
+            {
+                int c = b * b;
+                b = b + 1;
+                if (c <= n)
+                {
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
