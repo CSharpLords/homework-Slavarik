d2a71223a7c5da8e50bcef1c7239116de64bde0e
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-11-26 13:05:54 +0300

    НЛО

---
+++ 10/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                Console.WriteLine("Введите грузоподъёмность НЛО:");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите массу коровы");
+                double first = double.Parse("Введите массу коровы");
+                double second = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите массу коровы");
+                double third = double.Parse(Console.ReadLine());
+
+                if (a = first + second + third) {
+
+
+        }
+    }
+}
