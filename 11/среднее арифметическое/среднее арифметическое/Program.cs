197183173c58577e9288b1c99b3a28900f541620
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-02 20:06:24 +0300

    Среднее арифметическое

---
+++ 11/среднее арифметическое/среднее арифметическое/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace среднее_арифметическое
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int account = 0;
+            int number = 0;
+            int sum = 0;
+            while(number < 50)
+            {
+                Thread.Sleep(100);
+                Random rand = new Random();
+                int value = rand.Next(0, 200);
+                account = account + 1;
+                Console.WriteLine(account + ".Ваше число:" + value);
+                sum = sum + value;
+                number = number + 1;
+
+            }
+            Console.WriteLine("Среднее арифметическое чисел:" + sum/50);
+            Console.ReadLine();
+        }
+    }
+}
