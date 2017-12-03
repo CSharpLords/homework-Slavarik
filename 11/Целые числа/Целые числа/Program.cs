3e09d54e6155888390ca3860a4d5a35000f4ce2c
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-03 14:58:20 +0300

    Целые числа
    Переделал

--- 11/Целые числа/Целые числа/Program.cs
+++ 11/Целые числа/Целые числа/Program.cs
@@ -12,41 +12,23 @@ namespace Целые_числа
         static void Main(string[] args)
         {
             int account = 0;
-            int Value = 0;
-            int n = 0;
-            int number = 0;
-            int number1 = 0;
+            int c = 0;
+            int n = 7;
             Random rand = new Random();
-            while(n < 1)
+            int a = 15;
+            int b = n * a;
+            if (b > 100)
             {
-                number = rand.Next(100, 200);
-                int Ost1 = number % 7;                           //что-то новое...
-                if (Ost1 == 0)
+                while (c < 14)
                 {
-                    Thread.Sleep(250);
                     account = account + 1;
-                    Console.WriteLine(account + ".Число:" + number);
-                    Value = Value + 1;
-                    n = n + 1;
+                    Console.WriteLine(account + ".Ваше число:" + b);
+                    a = a + 1;
+                    b = n * a;
+                    c = c + 1;
                 }
             }
-
-            while (Value < 14)
-            {
-                number1 = rand.Next(100, 200);
-                int Ost1 = number1 % 7;                           
-                if(Ost1 == 0)
-                {
-                    if(number != number1)
-                    {
-                        Thread.Sleep(250);
-                        account = account + 1;
-                        Console.WriteLine(account + ".Число:" + number1);
-                        Value = Value + 1;
-                    }                                      
-                }
-            }
-            Console.ReadLine();    
+            Console.ReadLine();
         }
     }
 }
