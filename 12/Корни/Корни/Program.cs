5685ed43e4c50a576c93e8e46555a2a28c0b3607
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-08 17:32:48 +0300

    Квадратные корни

---
+++ 12/Корни/Корни/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Корни
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 0.0;
+
+            while(n < 0.9)
+            {
+                Console.WriteLine("Корень из:" + n + " = " + Math.Sqrt(n));
+                n = n + 0.1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
