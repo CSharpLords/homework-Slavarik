10a84b50ec4691c3b4d7fa40d4a3c45e0b095268
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-10 10:50:56 +0300

    Слова

---
+++ 12/Слова/Слова/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Слова
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+            string Word = Console.ReadLine();
+
+            for (int letter = 0; letter < Word.Length; letter = letter + 1)
+            {
+                int character = Word.Length - 1;
+                Console.WriteLine(Word[character - letter]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
