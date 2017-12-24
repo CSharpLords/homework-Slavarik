e737fe1fb2e9e781607845a75408cbeff86787bb
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-24 10:31:10 +0300

    Полож и Отр числа массива

---
+++ 14/Полож числа/Полож числа/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Полож_числа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 11, 5, -2, 3 };
+            Console.WriteLine("Введите номер числа");
+            int digit = int.Parse(Console.ReadLine());
+            if (numbers[digit] < 0)
+            {
+                Console.WriteLine("Выбранное вами число отрицательное");
+            }
+            if (numbers[digit] > 0)
+            {
+                Console.WriteLine("Выбранное вами число  положительно");
+            }
+            Console.ReadLine();
+        }
+    }
+}
