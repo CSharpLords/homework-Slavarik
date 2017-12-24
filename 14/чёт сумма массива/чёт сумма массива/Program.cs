bd862a87d97e981f6444506e0aeb765f55072630
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-24 14:58:46 +0300

    Чёт сумма массива

---
+++ 14/чёт сумма массива/чёт сумма массива/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] massiv = { 10, 5, 1, 3, 4 };
+            for (int quantum = 0; quantum < massiv.Length; quantum = quantum + 1)
+            {
+                sum = sum + massiv[quantum];
+            }
+            int remainder = sum % 2;
+            if (remainder == 0)
+            {
+                Console.WriteLine("Сумма элементов в массиве является чётным числом");
+            }
+            else
+            {
+                Console.WriteLine("Сумма элементов в массиве является нечётным числом!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
