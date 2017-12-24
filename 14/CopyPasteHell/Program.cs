93849f2634ad601bd5a5090b08d39c076d4dac77
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-24 12:23:37 +0300

    Исправил

--- 14/CopyPasteHell/Program.cs
+++ 14/CopyPasteHell/Program.cs
@@ -8,16 +8,11 @@ namespace CopyPasteHell {
 	class Program {
 		static void Main(string[] args) {
 			float[] numbers = { 765, 341, 1, -3, 0, 13, -200, 23, 44 };
-
-			numbers[0] = numbers[0] / 5;
-			numbers[1] = numbers[1] / 5;
-			numbers[2] = numbers[2] / 5;
-			numbers[3] = numbers[3] / 5;
-			numbers[4] = numbers[4] / 5;
-			numbers[5] = numbers[5] / 5;
-			numbers[6] = numbers[6] / 5;
-			numbers[7] = numbers[7] / 5;
-			numbers[8] = numbers[8] / 5;
+            for(int number = 0;number < 9;number = number + 1)
+            {
+                numbers[number] = numbers[number] / 5;
+            }
+			
 			Console.ReadLine();
 		}
 	}
