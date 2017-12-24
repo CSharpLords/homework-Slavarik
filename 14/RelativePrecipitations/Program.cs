c864f15ade561742d52668bd0b25410f5c5f87b8
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-24 12:27:18 +0300

    Легко исправил

--- 14/RelativePrecipitations/Program.cs
+++ 14/RelativePrecipitations/Program.cs
@@ -8,9 +8,9 @@ namespace RelativePrecipitations {
 	class Program {
 		static void Main(string[] args) {
 			int[] precipitations = { 23, 4545, 45, 2, -16, 765, 341, 1, -3, 0 };
-
+            int zero = precipitations[0];
 			for (int i = 0; i < precipitations.Length; i++) {
-				precipitations[i] = precipitations[i] - precipitations[0];
+				precipitations[i] = precipitations[i] - zero;
 				Console.WriteLine(precipitations[i]);
 			}
 			Console.ReadLine();
