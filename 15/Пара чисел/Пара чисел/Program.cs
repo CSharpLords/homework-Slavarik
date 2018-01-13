bc9c6f7f4389cc5b90584367d6c67dbba0f6e503
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-01-13 13:47:25 +0300

    Пара чисел

---
+++ 15/Пара чисел/Пара чисел/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Большие_Предыд_Числа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 6, 2, 2, 7 };
+            int behind = 0;
+            int next = 1;
+            while (next < numbers.Length)
+            {
+                if (numbers[next] == numbers[behind])
+                {
+                    Console.WriteLine(numbers[next]);
+                }
+                behind = behind + 1;
+                next = next + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
