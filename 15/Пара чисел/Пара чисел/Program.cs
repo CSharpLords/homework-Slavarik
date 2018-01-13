d9057cbb7cc0eaadcf330b4dab38c482c4733933
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-01-13 22:08:36 +0300

    Сделяль)

--- 15/Пара чисел/Пара чисел/Program.cs
+++ 15/Пара чисел/Пара чисел/Program.cs
@@ -11,10 +11,19 @@ namespace Большие_Предыд_Числа
         static void Main(string[] args)
         {
             int[] numbers = { -5, 6, 2, 2, 7 };
-                if (numbers[0] > 0 && numbers[1] > 0 || numbers[0] < 0 && numbers[1] < 0)
+            int n = 0;
+            int back = 0;
+            int next = 1;
+            while(n < 1)
+            {
+                if (numbers[back] > 0 && numbers[next] > 0 || numbers[back] < 0 && numbers[next] < 0 || numbers[back] == 0 && numbers[next] == 0)
                 {
-                    Console.WriteLine(numbers[0] + " " + numbers[1]);
+                    Console.WriteLine(numbers[back] + " " + numbers[next]);
+                    n = n + 1;
                 }
+                back = back + 1;
+                next = next + 1;
+            }    
 
             Console.ReadLine();
         }
