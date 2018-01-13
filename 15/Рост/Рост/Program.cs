85d0ade6d9dc647341493e3359b0d7ac8e458d7e
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-01-13 14:06:23 +0300

    Генератор Роста

---
+++ 15/Рост/Рост/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Рост
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] growth = new int[5];
+            Random rand = new Random();
+
+            Console.WriteLine("генерируем рост ребят");
+            for (int i = 0; i < growth.Length; i++)
+            {
+                growth[i] = rand.Next(163, 191);
+            }
+            for (int i = 0; i < growth.Length; i++)
+            {
+                Console.WriteLine(growth[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
