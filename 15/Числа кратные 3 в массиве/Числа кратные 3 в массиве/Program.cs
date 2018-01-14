3092d6d4791e33ca145cddf8d898bc7775051ced
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-01-14 11:10:49 +0300

    Числа кратные 3 в массиве

---
+++ 15/Числа кратные 3 в массиве/Числа кратные 3 в массиве/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Числа_кратные_3_в_массиве
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[60];
+            int n = 0;
+            int i = 60; 
+            while(i > 10)
+            {
+                numbers[n] = i;
+                Console.WriteLine(numbers[n]);
+                i = i - 3;
+            }
+            Console.ReadLine();
+        }   
+    }
+}
