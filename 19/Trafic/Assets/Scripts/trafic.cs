3cc56b0b8c489b885f249e02e1cc68459c0a0f66
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-02-18 15:52:32 +0300

    ПОПА(Километр на Километр)

---
+++ 19/Trafic/Assets/Scripts/trafic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trafic : MonoBehaviour {
+	public Transform target;
+
+	void Start () {
+	}
+
+	void Update () {
+		Vector3 direction  = target.position - transform.position;
+		direction.Normalize();
+		transform.Translate(direction * 5 *  Time.deltaTime);
+	}
+}
