5500d8600d3934f78077682118ce89b5b6f39072
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-02-18 16:40:03 +0300

    JOPA

---
+++ 19/MoreObjects/Assets/Scrips/Trafic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trafic : MonoBehaviour {
+	public Transform[] objects; 
+	float min = 1000;
+	Transform nearobj;
+	void Start () {
+		for (int n = 0; n < objects.Length; n++) {
+			float dist = Vector3.Distance(objects[n].position ,  transform.position);
+			if(dist < min){
+				nearobj = objects[n];
+				min = dist;
+			}
+		}
+	}
+	
+
+	void Update () {
+		Vector3 direction  = nearobj.position - transform.position;
+		direction.Normalize();
+		transform.Translate(direction * 5 *  Time.deltaTime);
+	}
+}
