dedc6d5a7f8a2395c7307a2ad8ed4e13a44b712a
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-03-11 16:44:32 +0300

    Футболъ

---
+++ 22/Football/Assets/Scrips/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+	public Button startButton;
+	public Button creditsButton;
+	public Button exitButton;
+
+	void LoadGame() {
+		SceneManager.LoadScene("Football2");
+	}
+	void ExitGame() {
+		Application.Quit();
+	}
+
+	void CreditsGame() {
+		SceneManager.LoadScene("Credits");
+	}
+
+	void Start () {
+		startButton.onClick.AddListener(LoadGame);
+		exitButton.onClick.AddListener(ExitGame);
+		creditsButton.onClick.AddListener (CreditsGame);
+	}
+	
+
+	void Update () {
+		
+	}
+}
