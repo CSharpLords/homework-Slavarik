791894c744be16868dee9e466ee22f92f9646460
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-04-08 16:24:24 +0300

    Сделано

--- 26/Hello/Program.cs
+++ 26/Hello/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Hello {
 	class Program {
+        static Random rand = new Random();
 		static void Main(string[] args) {
 			SayHello("Маша");
 			SayHello("Женя");
@@ -14,8 +15,20 @@ namespace Hello {
 			Console.ReadLine();
 		}
 
-		static void SayHello(string name) {
-			// пиши код здесь
+		 static void SayHello(string name) {
+            int number = rand.Next(0, 3);
+            if (number == 0)
+            {
+                Console.WriteLine("Привет," + name);
+            }
+            if (number == 1)
+            {
+                Console.WriteLine("Доброго ранку," + name);
+            }
+            if (number == 2)
+            {
+                Console.WriteLine("Hello," + name);
+            }
 		}
 	}
 }
